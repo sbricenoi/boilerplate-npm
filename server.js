824e259bd9491e184a519df28c6bd73fdd5c63f9
Sebastian Briceño <sbriceno@marsol.cl>
2024-01-28 19:19:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.route('/_api/package.json')
   */
 app.route('/').get(function(req, res) {
       console.log("Hello World")
-      res.send('Hello Express');
+		  res.sendFile(process.cwd() + '/views/index.html');
     })
 /*
 // Respond not found to all the wrong routes
