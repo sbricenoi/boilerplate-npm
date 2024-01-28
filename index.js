94e5075a830cc9f53c15e47471fa5b1e33c621f8
Sebastian Briceño <sbriceno@marsol.cl>
2024-01-28 15:17:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const PORT = process.env.PORT || 3000
-const http = require('http')
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  fs.readFile(__dirname + '/package.json', function(err, data) {
-    if(err) return next(err);
-    res.type('txt').send(data.toString());
+
+app.route('/')
+    .get(function(req, res) {
+		  res.sendFile(process.cwd() + '/views/index.html');
+    })
+    
+app.route('/_api/package.json')
+  .get(function(req, res, next) {
+    console.log('requested');
+    fs.readFile(__dirname + '/package.json', function(err, data) {
+      if(err) return next(err);
+      res.type('txt').send(data.toString());
+    });
   });
-})
 
-server.listen(PORT, () => {
-  console.log(`Server running on port:${PORT}/`)
-})
+//Listen on port set in environment variable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Node.js listening on port " + listener.address().port);
+});
